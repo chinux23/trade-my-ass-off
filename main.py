ca1fcd36d0c9e882db001cfd6182b84779477c31
Chen Huang <chen_huang@apple.com>
2017-09-01 08:13:27 -0400

    Update theme and fonts.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
     '''
     index page
     '''
-    return "<h1>Hello World</h1>"
+    return render_template('list.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
